eabbbd363acbaa1a4db2e065e218d24407f01af8
robi874 <62009463+robi874@users.noreply.github.com>
2020-11-30 21:41:32 +0100

    .

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-var teaPhrase = "Tea go sleep";
-
 $(document).ready(function() {
-    $('#yes').click(teaSleep());
-});
+	$('#ok').click(function (event){
+		var x = $('#person').val();
+		if(x == 'tea'){
+			teaSleep();
+		}
+		else if(x == 'jen'){
+			jenSleep();
+		}
+	});
+	$('#no').click(function (event){
+		kermitJudge();
+	});
+});	
+function jenSleep() {
+	$('#kermit').attr("src","eevee_pat.png");
+	$('h1').html("Nice jen go sleep");
+}
 
 function teaSleep() {
-    return new function(){
-        document.getElementById('titleTea').value = "peepoPog";
-    }
-
+	$('#kermit').attr("src","eevee_sleep.png");
+	$('h1').html("Nice tea go sleep");
+}
+function kermitJudge() {
+	$('#kermit').attr("src", "kermitjudge.png");
+	$('h1').html("kermitjudge");
+}
+function changePage(){
+	var x = $('#person').val();
+	if(x == 'tea'){
+		$('#kermit').attr("src", "");
+		$('h1').html("Tea go sleep");
+	}
+	else if(x == 'jen'){
+		$('#kermit').attr("src", "");
+		$('h1').html("Jen go sleep");
+	}
+	
 }
